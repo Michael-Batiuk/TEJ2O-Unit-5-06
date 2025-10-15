175403c13c5ae9a07b08caf4aaaf330710edf952
Michael-Batiuk <Michael.Batiuk@stu.ocsb.ca>
2025-10-15 13:28:07 -0400

    Shows Distance

--- main.ts
+++ main.ts
@@ -1,8 +1,25 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Michael Batiuk
+ * Created on: Oct 2025
+ * This program shows distance
 */
 
-basic.showString('Hello, World!')
+// variables
+let distanceToObject : number = 0
+
+// setup
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+// find distance from sonar
+input.onButtonPressed(Button.A, function() {
+    basic.clearScreen()
+    distanceToObject = sonar.ping(
+        DigitalPin.P1,
+        DigitalPin.P2,
+        PingUnit.Centimeters
+        )
+        basic.showNumber(distanceToObject)
+        basic.showIcon(IconNames.Happy)
+})
